c42de75a05748983f8814f626a05d98fff4db528
Mike Jarmy <mjarmy@gmail.com>
2022-09-08 06:46:02 -0400

    refactor

--- src/GAIN.cpp
+++ src/GAIN.cpp
@@ -1,7 +1,7 @@
 #include "dsp.hpp"
 #include "plugin.hpp"
 #include "widgets.hpp"
-// include "vu.hpp"
+#include "vu.hpp"
 
 // define GAIN_DEBUG
 
@@ -76,96 +76,6 @@ struct GAIN : Module {
 };
 
 //--------------------------------------------------------------
-// VUMeter
-//--------------------------------------------------------------
-
-struct VUMeter : OpaqueWidget {
-
-    const NVGcolor red = nvgRGB(0xE6, 0x29, 0x34);
-    const NVGcolor orange = nvgRGB(0xFF, 0x87, 0x24);
-    const NVGcolor yellow = nvgRGB(0xFF, 0xCA, 0x33);
-    const NVGcolor green = nvgRGB(0x3E, 0xD5, 0x64);
-
-    // TODO make a base class that we can get VULevels from
-    GAIN* module;
-
-    VUMeter(GAIN* module_) : module(module_) {}
-
-    void draw(const DrawArgs& args) override {
-
-        float leftRms = 0.0;
-        float rightRms = 0.0;
-        if (module) {
-            leftRms = module->vuLevels.leftRms;
-            rightRms = module->vuLevels.rightRms;
-        }
-
-        drawLevel(args, 0, leftRms);
-        drawLevel(args, 5, rightRms);
-    }
-
-    void drawLevel(const DrawArgs& args, float x, float level) {
-
-        float db = clamp(ampToDb(level / 10.0f), -120.0f, 6.0f);
-        if (db < -71.0f) {
-            return;
-        }
-
-        NVGpaint redOrange = nvgLinearGradient(args.vg, 0, 0, 0, 30, red, orange);
-        NVGpaint yellowGreen = nvgLinearGradient(args.vg, 0, 45, 0, 60, yellow, green);
-
-        drawSegment(args, x, db, 0.0f, 6.0f, 30, 0, NVGcolor{}, redOrange, false);
-        drawSegment(args, x, db, -3.0f, 0.0f, 45, 30, yellow, NVGpaint{}, true);
-        drawSegment(args, x, db, -6.0f, -3.0f, 60, 45, NVGcolor{}, yellowGreen, false);
-        drawSegment(args, x, db, -12.0f, -6.0f, 81, 60, green, NVGpaint{}, true);
-        drawSegment(args, x, db, -24.f, -12.0f, 102, 81, green, NVGpaint{}, true);
-        drawSegment(args, x, db, -48.f, -24.0f, 123, 102, green, NVGpaint{}, true);
-        drawSegment(args, x, db, -72.f, -48.0f, 144, 123, green, NVGpaint{}, true);
-    }
-
-    void drawSegment(
-        const DrawArgs& args,
-        float x,
-        float db,
-        float lowDb,
-        float highDb,
-        float bottom,
-        float top,
-        NVGcolor color,
-        NVGpaint gradient,
-        bool isColor) {
-
-        if (db < lowDb) {
-            return;
-        }
-
-        float y = (db > highDb) ? top : rescale(db, lowDb, highDb, bottom, top);
-        float height = bottom - y;
-        drawRect(args, x, y, 3, height, color, gradient, isColor);
-    }
-
-    void drawRect(
-        const DrawArgs& args,
-        float x,
-        float y,
-        float w,
-        float h,
-        NVGcolor color,
-        NVGpaint gradient,
-        bool isColor) {
-
-        nvgBeginPath(args.vg);
-        nvgRect(args.vg, x, y, w, h);
-        if (isColor) {
-            nvgFillColor(args.vg, color);
-        } else {
-            nvgFillPaint(args.vg, gradient);
-        }
-        nvgFill(args.vg);
-    }
-};
-
-//--------------------------------------------------------------
 // GAINWidget
 //--------------------------------------------------------------
 
@@ -192,7 +102,10 @@ struct GAINWidget : ModuleWidget {
         addInput(createInputCentered<MPort>(Vec(22.5, 279), module, GAIN::kInput));
         addOutput(createOutputCentered<MPort>(Vec(22.5, 320), module, GAIN::kOutput));
 
-        VUMeter* meter = new VUMeter(module);
+        VUMeter* meter = new VUMeter();
+        if (module) {
+            meter->vuLevels = &(module->vuLevels);
+        }
         meter->box.pos = Vec(22.5 - 9, 40);
         meter->box.size = Vec(9, 144);
         addChild(meter);
--- src/vu.hpp
+++ src/vu.hpp
@@ -1,48 +1,134 @@
-//#pragma once
-//
-//#include "rack.hpp"
-//
-//#include "dsp.hpp"
-//
-// using namespace rack;
-//
-//
-// struct VUMeter : OpaqueWidget {
-//
-//    const float height = 144.0f;
-//    const NVGcolor green = nvgRGB(0x3E, 0xD5, 0x64);
-//
-//    // TODO make a base class that we can get VULevels from
-//    GAIN* module = NULL;
-//
-//	VUMeter(GAIN* module_) : module(module_) {}
-//
-//	void draw(const DrawArgs& args) override {
-//        float leftRms = 0.0;
-//		if (module) {
-//            // TODO make a base class that we can get VULevels from
-//            leftRms = module->levels.leftRms;
-//        }
-//
-//        float db = clamp(ampToDb(leftRms/10.0f), -120.0f, 6.0f);
-//        if (db < -119.0f) {
-//            return;
-//        }
-//        float y = rescale(db, -120.0f, 6.0f, height, 0.0f);
-//
-//        nvgBeginPath(args.vg);
-//        nvgRect(args.vg, 0, y, 3, height - y);
-//        nvgFillColor(args.vg, green);
-//        nvgFill(args.vg);
-//
-//        //nvgBeginPath(args.vg);
-//        //nvgRect(args.vg, 5, 0, 3, 144);
-//        //nvgFillColor(args.vg, green);
-//        //nvgFill(args.vg);
-//
-//        //nvgBeginPath(args.vg);
-//        //nvgRect(args.vg, 5, 0, 8, 144);
-//        //nvgFillColor(args.vg, green);
-//        //nvgFill(args.vg);
-//    }
-//};
+#pragma once
+
+#include "rack.hpp"
+
+#include "dsp.hpp"
+
+using namespace rack;
+
+//--------------------------------------------------------------
+// VuLevels
+//--------------------------------------------------------------
+
+// This class is adapted from /Rack-SDK/leftclude/dsp/vumeter.hpp.
+// License is GPL3.
+struct VULevels {
+
+    // TODO have a look at this for more sophistacted approach, e.g. circular buffer for RMS
+    // https://www.kvraudio.com/forum/viewtopic.php?t=460756
+
+    static constexpr float lambda = 30.f; // Inverse time constant left 1/seconds
+
+    float leftRms = 0.0f;
+    float leftPeak = 0.0f;
+    float rightRms = 0.0f;
+    float rightPeak = 0.0f;
+
+    void process(float left, float right, float deltaTime) {
+
+        leftRms += (left * left - leftRms) * lambda * deltaTime;
+
+        rightRms += (right * right - rightRms) * lambda * deltaTime;
+
+        float leftAbs = std::fabs(left);
+        if (leftAbs >= leftPeak) {
+            leftPeak = leftAbs;
+        } else {
+            leftPeak += (leftAbs - leftPeak) * lambda * deltaTime;
+        }
+
+        float rightAbs = std::fabs(right);
+        if (rightAbs >= rightPeak) {
+            rightPeak = rightAbs;
+        } else {
+            rightPeak += (rightAbs - rightPeak) * lambda * deltaTime;
+        }
+    }
+};
+
+//--------------------------------------------------------------
+// VUMeter
+//--------------------------------------------------------------
+
+struct VUMeter : OpaqueWidget {
+
+    const NVGcolor red = nvgRGB(0xE6, 0x29, 0x34);
+    const NVGcolor orange = nvgRGB(0xFF, 0x87, 0x24);
+    const NVGcolor yellow = nvgRGB(0xFF, 0xCA, 0x33);
+    const NVGcolor green = nvgRGB(0x3E, 0xD5, 0x64);
+
+    VULevels* vuLevels = NULL;
+
+    void draw(const DrawArgs& args) override {
+        if (!vuLevels) {
+            return;
+        }
+
+        float leftRms = vuLevels->leftRms;
+        float rightRms = vuLevels->rightRms;
+
+        drawLevel(args, 0, leftRms);
+        drawLevel(args, 5, rightRms);
+    }
+
+    void drawLevel(const DrawArgs& args, float x, float level) {
+
+        float db = clamp(ampToDb(level / 10.0f), -120.0f, 6.0f);
+        if (db < -71.0f) {
+            return;
+        }
+
+        NVGpaint redOrange = nvgLinearGradient(args.vg, 0, 0, 0, 30, red, orange);
+        NVGpaint yellowGreen = nvgLinearGradient(args.vg, 0, 45, 0, 60, yellow, green);
+
+        drawSegment(args, x, db, 0.0f, 6.0f, 30, 0, NVGcolor{}, redOrange, false);
+        drawSegment(args, x, db, -3.0f, 0.0f, 45, 30, yellow, NVGpaint{}, true);
+        drawSegment(args, x, db, -6.0f, -3.0f, 60, 45, NVGcolor{}, yellowGreen, false);
+        drawSegment(args, x, db, -12.0f, -6.0f, 81, 60, green, NVGpaint{}, true);
+        drawSegment(args, x, db, -24.f, -12.0f, 102, 81, green, NVGpaint{}, true);
+        drawSegment(args, x, db, -48.f, -24.0f, 123, 102, green, NVGpaint{}, true);
+        drawSegment(args, x, db, -72.f, -48.0f, 144, 123, green, NVGpaint{}, true);
+    }
+
+    void drawSegment(
+        const DrawArgs& args,
+        float x,
+        float db,
+        float lowDb,
+        float highDb,
+        float bottom,
+        float top,
+        NVGcolor color,
+        NVGpaint gradient,
+        bool isColor) {
+
+        if (db < lowDb) {
+            return;
+        }
+
+        float y = (db > highDb) ? top : rescale(db, lowDb, highDb, bottom, top);
+        float height = bottom - y;
+        drawRect(args, x, y, 3, height, color, gradient, isColor);
+    }
+
+    void drawRect(
+        const DrawArgs& args,
+        float x,
+        float y,
+        float w,
+        float h,
+        NVGcolor color,
+        NVGpaint gradient,
+        bool isColor) {
+
+        nvgBeginPath(args.vg);
+        nvgRect(args.vg, x, y, w, h);
+        if (isColor) {
+            nvgFillColor(args.vg, color);
+        } else {
+            nvgFillPaint(args.vg, gradient);
+        }
+        nvgFill(args.vg);
+    }
+};
+
