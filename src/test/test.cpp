da5c23c7b310d43b84b9d72bd3950e9fcb3bfb30
Mike Jarmy <mjarmy@gmail.com>
2022-09-10 15:08:25 -0400

    play with test suite

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -6,10 +6,6 @@
 
 #include "../../lib/bogaudio/dsp/signal.hpp"
 
-float dist(float x, float low, float high) {
-    return (x - low) / (high - low);
-}
-
 void dumpFloat(float v) {
     std::cout << std::fixed;
     std::cout << std::setprecision(2);
@@ -19,17 +15,16 @@ void dumpFloat(float v) {
 int main() {
 
     const float sampleRate = 48000.0f;
-    const float kMinDb = -72.0f;
-    const float kMaxDb = 6.0f;
+    const float slewTime = 5.0f; // millis
 
-	bogaudio::dsp::SlewLimiter dbSlew;
-    dbSlew.setParams(sampleRate, 5.0f, kMaxDb - kMinDb);
+	bogaudio::dsp::SlewLimiter slew;
+    slew.setParams(sampleRate, slewTime, 1.0f);
 
     std::cout << "-----------------------------------" << std::endl;
 
+    slew.setLast(0.9); 
     for (int i = 0; i < 100; i++) {
-        //dumpFloat(dbSlew.next(kMinDb));
-        dumpFloat(dbSlew.next(6.0f));
+        dumpFloat(slew.next(1.1f));
     }
     
     std::cout << "-----------------------------------" << std::endl;
