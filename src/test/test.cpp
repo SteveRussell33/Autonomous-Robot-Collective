874bc236f97a697d0cf98f1f5770fa6e339c75a7
Mike Jarmy <mjarmy@gmail.com>
2022-09-11 15:35:12 -0400

    cleanup

--- src/test/test.cpp
+++ src/test/test.cpp
@@ -27,38 +27,19 @@ void dump(float v) {
 //    }
 //}
 
-static constexpr float kTwoPi = 2.0f * M_PI;
-
-// This wavefolding algorithm is derived from a Max/MSP patch that was
-// created by Randy Jones of Madrona Labs.
-inline float wavefold(float in, float timbre) {
-
-    float ampOffset = timbre * 2.0f + 0.1f;
-    float phaseOffset = timbre + 0.25f;
-
-    float phase = in * ampOffset;
-    phase = phase + phaseOffset;
-
-    // TODO should we use a fast approximation for cosf()?
-    return std::cosf(kTwoPi * (phase + 0.75));
-}
-
-void fold() {
+void csv() {
 
     std::cout << "T,A,B" << std::endl;
-
-    float timbre = 0.4f;
-    float amplitude = 0.1f;
 
     for (int i = 0; i < 10; i++) {
         for (int j = 0; j < 100; j++) {
 
             float t = i + j/100.0f;
 
-            float a = std::sinf(kTwoPi * t);
+            float a = foo();
             a = a * amplitude;
 
-            float b = wavefold(a, timbre);
+            float b = bar();
             b = b * amplitude;
 
             dump(t);
@@ -68,11 +49,9 @@ void fold() {
             dump(b);
             std::cout << std::endl;
         }
-        
-        //amplitude = amplitude - 1.0f/20.0f;
     }
 }
 
 int main() {
-    fold();
+    csv();
 }
