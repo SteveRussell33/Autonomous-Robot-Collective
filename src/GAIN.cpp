3f6bd7539862bb32b7e1c5424c0df8cdc2a8acc7
Mike Jarmy <mjarmy@gmail.com>
2022-09-26 12:30:40 -0400

    work on GAIN refactor

--- src/GAIN.cpp
+++ src/GAIN.cpp
@@ -10,15 +10,14 @@
 
 struct GAIN : Module {
 
-    StereoTrack track;
+    //StereoTrack track;
 
     enum ParamId { kLevelParam, kMuteParam, kParamsLen };
 
-    enum InputId { kLeftInput, kRightInput, kLevelCvInput, kInputsLen };
+    enum InputId { kInput, kLevelCvInput, kInputsLen };
 
     enum OutputId {
-        kLeftOutput,
-        kRightOutput,
+        kOutput,
 
 #ifdef GAIN_DEBUG
         kDebug1,
@@ -36,11 +35,9 @@ struct GAIN : Module {
         configInput(kLevelCvInput, "Level CV");
         configSwitch(kMuteParam, 0.f, 1.f, 0.f, "Mute", {"Off", "On"});
 
-        configInput(kLeftInput, "Left");
-        configInput(kRightInput, "Right");
+        configInput(kInput, "Audio");
 
-        configOutput(kLeftOutput, "Left");
-        configOutput(kRightOutput, "Right");
+        configOutput(kOutput, "Audio");
 
 #ifdef GAIN_DEBUG
         configOutput(kDebug1, "Debug 1");
@@ -48,38 +45,27 @@ struct GAIN : Module {
         configOutput(kDebug3, "Debug 3");
         configOutput(kDebug4, "Debug 4");
 #endif
-
-        //------------------------------------------------------
-
-        track.init(
-            &(inputs[kLeftInput]),
-            &(inputs[kRightInput]),
-            &(params[kLevelParam]),
-            &(inputs[kLevelCvInput]),
-            NULL,
-            NULL);
     }
 
     void onSampleRateChange(const SampleRateChangeEvent& e) override {
-        track.onSampleRateChange(e.sampleRate);
+        //track.onSampleRateChange(e.sampleRate);
     }
 
-    void processOutput(MonoTrack& trk, Output& output) {
-        if (output.isConnected()) {
-            output.setChannels(trk.output.channels);
-            output.writeVoltages(trk.output.voltages);
-        } else {
-            output.setChannels(0);
-        }
-    }
+    //void processOutput(MonoTrack& trk, Output& output) {
+    //    if (output.isConnected()) {
+    //        output.setChannels(trk.output.channels);
+    //        output.writeVoltages(trk.output.voltages);
+    //    } else {
+    //        output.setChannels(0);
+    //    }
+    //}
 
     void process(const ProcessArgs& args) override {
 
-        bool muted = params[kMuteParam].getValue() > 0.5f;
-        track.process(args.sampleTime, muted);
+        //bool muted = params[kMuteParam].getValue() > 0.5f;
+        //track.process(args.sampleTime, muted);
 
-        processOutput(track.left, outputs[kLeftOutput]);
-        processOutput(track.right, outputs[kRightOutput]);
+        //processOutput(track.left, outputs[kOutput]);
     }
 };
 
@@ -103,26 +89,23 @@ struct GAINWidget : ModuleWidget {
         addOutput(createOutputCentered<ArcPolyPort>(Vec(12, 84), module, GAIN::kDebug4));
 #endif
 
-        addMeter(24 - 6, 44, module ? &(module->track.left.vuStats) : NULL);
-        addMeter(24 + 1, 44, module ? &(module->track.right.vuStats) : NULL);
+        //addMeter(24 - 6, 44, module ? &(module->track.left.vuStats) : NULL);
+        //addMeter(24 + 1, 44, module ? &(module->track.right.vuStats) : NULL);
 
-        addParam(createParamCentered<ArcKnob24>(Vec(24, 166), module, GAIN::kLevelParam));
-        addInput(createInputCentered<ArcPolyPort>(Vec(24, 196), module, GAIN::kLevelCvInput));
-        addParam(createParamCentered<ArcMuteButton>(Vec(24, 226), module, GAIN::kMuteParam));
+        addParam(createParamCentered<ArcKnob24>(Vec(22.5, 166), module, GAIN::kLevelParam));
+        addInput(createInputCentered<ArcPolyPort>(Vec(22.5, 196), module, GAIN::kLevelCvInput));
+        addParam(createParamCentered<ArcMuteButton>(Vec(22.5, 226), module, GAIN::kMuteParam));
 
-        addInput(createInputCentered<ArcPolyPort>(Vec(24, 256), module, GAIN::kLeftInput));
-        addInput(createInputCentered<ArcPolyPort>(Vec(24, 286), module, GAIN::kRightInput));
-
-        addOutput(createOutputCentered<ArcPolyPort>(Vec(24, 316), module, GAIN::kLeftOutput));
-        addOutput(createOutputCentered<ArcPolyPort>(Vec(24, 346), module, GAIN::kRightOutput));
+        addInput(createInputCentered<ArcPolyPort>(Vec(22.5, 293), module, GAIN::kInput));
+        addOutput(createOutputCentered<ArcPolyPort>(Vec(22.5, 334), module, GAIN::kOutput));
     }
 
-    void addMeter(float x, float y, VuStats* vuStats) {
-        VuMeter* meter = new VuMeter(vuStats);
-        meter->box.pos = Vec(x, y);
-        meter->box.size = Vec(8, 104);
-        addChild(meter);
-    }
+    //void addMeter(float x, float y, VuStats* vuStats) {
+    //    VuMeter* meter = new VuMeter(vuStats);
+    //    meter->box.pos = Vec(x, y);
+    //    meter->box.size = Vec(8, 104);
+    //    addChild(meter);
+    //}
 };
 
 Model* modelGAIN = createModel<GAIN, GAINWidget>("GAIN");
